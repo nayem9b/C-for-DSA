53b19645863bd7f5f4a8d9605574142b84dbcbff
Md Razwan Niam <razwanniam1@gmail.com>
2023-11-12 19:07:02 +0600

    iostream and endl

--- output.cpp
+++ output.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 int main (){
-    int a=10;
+    int a=10, b=20;
     std :: cout <<"Starting CPP!\n";
-    std :: cout << a;
-    std :: cout << "My Number is" << a;
+    // std :: cout << a;
+    // std :: cout << "My Number is" << a;
+    std :: cout <<a << std::endl <<b << std::endl;
+    std :: cout <<a << std::endl <<b;
     return 0;
 }
