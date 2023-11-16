56f6cd0ed3de33725d3e260b7147d5a49814abbf
nayem9b <razwanniam1@gmail.com>
2023-11-16 22:24:16 +0600

    ascending order

--- Class and Object/sort_array.cpp
+++ Class and Object/sort_array.cpp
@@ -9,7 +9,7 @@ int main()
     {
         cin >> a[i];
     }
-    // sort(a, a + n); // ascending
+    sort(a, a + n); 
     sort(a, a + n, greater<int>()); // descending
     for (int i = 0; i < n; i++)
     {
