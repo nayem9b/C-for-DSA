1c85470eb5b1fc45a61e9a6328b33630eb634ad9
Md Razwan Niam <razwanniam1@gmail.com>
2023-11-27 20:45:14 +0600

    choose three problem solved

---
+++ Monkey.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char s[100];
+    while (cin.getline(s, 100))
+    {
+        for (int i = 0; i < s[100]; i++)
+        {
+            cout << s[i];
+        }
+
+        // cout << s << endl;
+    }
+    return 0;
+}
