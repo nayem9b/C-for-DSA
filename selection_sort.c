e3e7684598dbf97f893d282db09532b50cfb5d47
Md Razwan Niam <razwanniam1@gmail.com>
2023-11-27 15:52:59 +0600

    sorting problem solved

---
+++ selection_sort.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    // ascending
+    // for(int i=0;i<n-1;i++)
+    // {
+    //     for(int j=i+1;j<n;j++)
+    //     {
+    //         if(a[i]>a[j])
+    //         {
+    //             int tmp=a[i];
+    //             a[i]=a[j];
+    //             a[j]=tmp;
+    //         }
+    //     }
+    // }
+    // descending
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] < a[j])
+            {
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int a[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> a[i];
+//     }
+//     // sort(a, a + n); // ascending
+//     sort(a, a + n, greater<int>()); // descending
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << a[i] << " ";
+//     }
+//     return 0;
+// }
